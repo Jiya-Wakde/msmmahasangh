539ac3aac8c900e9f01a0a658980e36935ad89d7
PrachiteshWakde <ppwcopy@gmail.com>
2024-09-28 21:05:44 +0530

    Environment Variable Updated

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///user.db")
 
 # Initialize extensions
 csrf = CSRFProtect(app)
